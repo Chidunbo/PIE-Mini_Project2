801b01889cb3ec03b7ade7de7962883b5d468b52
BrooklynWakefield <brooklynw02840@gmail.com>
2022-09-20 20:55:23 -0400

    Save camera data after read in python

--- test1.py
+++ test1.py
@@ -1,15 +1,15 @@
 import serial
 import csv
-with open('data.csv','w') as writeobj:
-    write = csv.writer(writeobj)
-    write.writerow(rows)
+#with open('data.csv','w') as writeobj:
+    #write = csv.writer(writeobj)
+    #write.writerow(rows)
     
 arduinoComPort = "COM4"
 baudRate = 9600
 serialPort = serial.Serial(arduinoComPort,baudRate,timeout=0)
+rawdata = []
 
 while True:
-    lineofData = serialPort.readline().decode()
-    if len(lineofData) > 0:
-    
+    #lineofData = serialPort.readline().decode()
+    rawdata.append(str(serialPort.readline()))
     
